930b8d2be05e34a37497666032151ceb6f721d00
Evgen Koromyslov <eok8177@gmail.com>
2015-10-17 22:46:09 +0300

    add parse body

--- index.php
+++ index.php
@@ -11,23 +11,17 @@ require_once 'DiDom/Query.php';
 
 use DiDom\Document;
 
-$url = 'http://www.central-group.ru/vyhledavani-byty.aspx?formular=rozsirene&cisloStranky=1&nic=on&nic=on&f1cena1=&f1cena2=&f1Dokoncene1=on&f1Dokoncene2=2017-06-30&f1NastaveniVypisu=1&f1NastaveniVypisuPocet=50&f2Lokalita_h1=9&f2Lokalita_h1_o9=10&f2Lokalita_h1_o9_c10=3281&f2Lokalita_h1_o9=27&f2Lokalita_h1_o9_c27=3315&f2Lokalita_h1_o9=46&f2Lokalita_h1_o9_c46=3322&nic=on&nic=on&nic=1&nic=0&f2patro1=x&f2patro2=x&nic=on&f2cena1=&f2cena2=&f2Dokoncene1=on&f2Dokoncene2=2017-06-30&f2NastaveniVypisu=1&f2NastaveniVypisu=0&f2NastaveniVypisu=0&f2NastaveniVypisuPocet=90';
+// $url = 'http://www...';
+$url = '01.html';
 
 $document = new Document($url, true);
 
-// $cities = $document
-//     ->find('thead tr')[0]
-//     ->find('td');
 $head = parse_head($document);
+file_put_contents('test.txt', $head);
 echo "<pre>".print_r($head, true)."</pre>";
-// echo 'Total cities: ' . count($cities) . '<br>';
 
-// echo "test: <pre>".print_r($cities, true);
-
-// foreach ($cities as $city) {
-//     echo $city->text() . '<br>';
-// }
-
+$content = parse($document, $head);
+echo "<pre>".print_r($content, true)."</pre>";
 
 
 function parse_head($doc)
@@ -42,7 +36,6 @@ function parse_head($doc)
 		$second_line[$i] = preg_replace("/\s/",'',$el);
 		$i++;
 	}
-	// echo "<pre>".print_r($second_line, true)."</pre>";
 
 	//Parse First line <tr>
 	$i = 1;
@@ -65,184 +58,145 @@ function parse_head($doc)
 	return $head;
 }
 
-public function parse($parser_id, $insert = false, $head = false)
+function parse($doc = false, $head = false)
 	{
 		end($head);
 		$num_last_td = key($head);
 
 		$i = 1;
-		$data = $this->initData();
+		$list = array();
+		$data = array();
 
-		foreach ($this->doc->find('tbody tr.radek td.bunka') as $_el) {
-			$el = pq($_el)->html();
+		foreach ($doc->find('tbody tr.radek td.bunka') as $element) {
 
 			switch ((string)$head[$i]) {
-				case 'Stav': //Состояние
-					// echo "$i = $head[$i]";
-					$data['active_parser'] = 1;
-					$el = '';
+				case 'Состояние': //condition
+					$data['active'] = 1;
+					$element = $element->find("div.ikony1 img");
 					for ($j = 0; $j < 3; $j++) {
-						$element = pq($_el)->find("div.ikony1 img:eq($j)")->attr('onmouseover');
-						if ($element) {
-							$element = preg_replace("/.*CAPTION, '/", "", $element);
-							$element = preg_replace("/', FGCOLOR.*/", "", $element);
-							if ($el) {
-								$el .= ', ' . $element;
-							} else {
-								$el = $element;
-							}
-							if ((string)$element == 'V jednání o prodeji s jiným zájemcem'){
-								//Зарезервировано
-								$data['active_parser'] = 0;
-								// echo "Status: $element <br>";
+						$el = $element[$j]->getAttribute('onmouseover');
+						if ($el) {
+							$el = preg_replace("/.*CAPTION, '/", "", $el);
+							$el = preg_replace("/', FGCOLOR.*/", "", $el);
+							if ((string)$el == 'Резервация (ведутся переговоры с другим клиентом)'){
+								//Reserved
+								$data['active'] = 0;
 							}
 						}
 					}
-					$data['memo_admin'] = $el;
 					break;
 
-				case 'Obec – Část obceNázev lokalita': //Населенный пункт – Район (Название проекта)
-					// echo "$i = $head[$i]";
-					$el = pq($_el)->find('div')->text();
-					$el = trim(preg_replace('/\s{2,}/', ' ', $el)); //убрать пробелы
+				case 'Населенный пункт – РайонНазвание проекта': //Locality
+					$el = $element->find('div')[0]->text();
+					$el = trim(preg_replace('/\s{2,}/', ' ', $el));
+					$data['locality'] = $el;
 					break;
 
-			//!!! $data['property_number']= корпус.этаж.В.квартира
-				case 'Byt. dům,podlaží': //Корпус, этаж
-					// echo "$i = $head[$i]";
-					$el = pq($_el)->find('span.vetsi-pismo')->text();
-					$el = preg_replace("/\D/", "", $el); //Вытянуть только цифры
-					$data['floor_num'] = (int)$el;
+				case 'Корпус, этаж': //floor
+					$el = $element->find('span.vetsi-pismo')[0]->text();
+					$data['floor'] = (int)preg_replace("/\D/", "", $el); //only numbers
 
-					$el = pq($_el)->find('b')->text(); //Номер дома (корпус)
-					$data['property_number'] = $el . $data['floor_num'];
+					$el = $element->find('b')[0]->text(); //num of house
+					$data['number'] = $el . $data['floor'];
 					break;
 
-				case 'Číslobytu': //Номер квартиры
-					// echo "$i = $head[$i]";
-					$el = (int)$el;
-					$data['property_number'] .= "B".$el;
-					// $el = $data['property_number'];
+				case 'Номерквартиры': //Apartment number
+					$data['number'] .= "B".(int)$element;
 					break;
 
-				case 'Dispozice, příslušenství': //Планировка, дополнительные удобства
-					// echo "$i = $head[$i]";
-					$el = pq($_el)->find('div.typ-bytu')->text();
-					$el = preg_replace("/\D/", "", $el); //Вытянуть только цифры
-					if (($rooms = substr("$el", -1)) !== false) {
-						$el = $rooms;
-					}
-					$data['planning_id'] = $el;
+				case 'Планировка, дополнительные удобства': //planing
+					$el = $element->find('div.typ-bytu')[0]->text();
+					$el = preg_replace("/\D/", "", $el); //only numbers
+					$data['planning'] = $el;
 
-					// ---- Атрибуты вытянуть ---
-					$el = false;
+					$others = false;
+					$element = $element->find("div.ikony2 img");
 					for ($j = 0; $j < 6; $j++) {
-						$element = pq($_el)->find("div.ikony2 img:eq($j)")->attr('onmouseover');
-						if ($element) {
-							$element = preg_replace("/.*CAPTION, '/", "", $element);
-							$element = preg_replace("/', FGCOLOR.*/", "", $element);
-							if ($el) {
-								$el .= ', ' . $element;
+						$el = $element[$j]->getAttribute('onmouseover');
+						if ($el) {
+							$el = preg_replace("/.*CAPTION, '/", "", $el);
+							$el = preg_replace("/', FGCOLOR.*/", "", $el);
+							if ($others) {
+								$others .= ', ' . $el;
 							} else {
-								$el = $element;
+								$others = $el;
 							}
 						}
 					}
-					if ($el) {
-						$data['memo_admin'] .= $el;
+					if ($others) {
+						$data['others'] .= $others;
 					}
 					break;
 
-				case 'Vnitřníužitnáplocha(m2)': //Жилая площадь
-					// echo "$i = $head[$i]";
-					$el = (float)str_replace(',', '.', $el);
-					$data['area_living'] = $el;
+				case 'Общаяжилаяплощадь(м²)': //Living area
+					$el = $element->text();
+					$data['area_living'] = (float)str_replace(',', '.', $el);
 					break;
 
-				case 'Celkovápodlahováplocha(m2)': //Общая площадь
-					// echo "$i = $head[$i]";
-					$el = pq($_el)->find('span')->text();
-					$el = (float)str_replace(',', '.', $el);
-					$data['area_total'] = $el;
+				case 'Общаяплощадь(по контуруобъекта)(м²)': //Total area
+					$el = $element->text();
+					$data['area_total'] = (float)str_replace(',', '.', $el);
 					break;
 
-				case 'Cenavnitřní užitnéplochy bytu(Kč/m2)': //Цена за метр
-					// echo "$i = $head[$i]";
-					$el = (int)str_replace('.', '', $el);
+				case 'Ценажилой площадиквартиры(в чеш.кронах/м²)': //price of m2
+					$el = $element->text();
+					$data['price_m'] = (int)str_replace('.', '', $el);
 					break;
 
-				case 'Cenaurčenýchpříplatkovýchpoložek(Kč)': //Цена дополнительных элементов отделки (чеш.крон без НДС)
-					// echo "$i = $head[$i]";
+				case 'Ценадополнительныхэлементов отделки(чеш.крон без НДС)': //price
+					$el = $element->text();
+					$data['price_plus'] = (int)str_replace('.', '', $el);
 					break;
 
-				case '(Kčbez DPH)': //Полная цена Без НДС
-					// echo "$i = $head[$i]";
-					$el = (int)str_replace('.', '', $el);
-					$data['price'] = $el;
+				case '(без НДС)': //full price without DPH
+					$el = $element->text();
+					$data['price'] = (int)str_replace('.', '', $el);
 					break;
 
-				case '(Kč s DPH)': //Полная цена с НДС
-					// echo "$i = $head[$i]";
-					$el = (int)str_replace('.', '', $el);
-					$data['price_inc_dph'] = $el;
+				case '(с НДС)': //full price with DPH
+					$el = $element->text();
+					$data['price_inc_dph'] = (int)str_replace('.', '', $el);
 					break;
 
-				case 'Termíndokončení': //Срок сдачи
-					// echo "$i = $head[$i]";
-					$el = pq($_el)->text();
-					$el = trim(preg_replace('/\s{2,}/', ' ', $el)); //убрать пробелы
+				case 'Сроксдачи': //date
+					$el = $element->text();
+					$data['date'] = trim(preg_replace('/\s{2,}/', ' ', $el));
 					break;
 
-				case 'Orientaceobytnýchmístností': //Ориентация
-					// echo "$i = $head[$i]";
-					$el = '';
+				case 'Сторонысвета,кудавыходяткомнаты': //orientation
+					$others = '';
+					$element = $element->find("img");
 					for ($j = 0; $j < 4; $j++) {
-						$element = pq($_el)->find("img:eq($j)")->attr('onmouseover');
-						if ($element) {
-							$element = preg_replace("/.*CAPTION, '/", "", $element);
-							$element = preg_replace("/', FGCOLOR.*/", "", $element);
-							if ($el) {
-								$el .= ', ' . $element;
+						$el = $element[$j]->getAttribute('onmouseover');
+						if ($el) {
+							$el = preg_replace("/.*CAPTION, '/", "", $el);
+							$el = preg_replace("/', FGCOLOR.*/", "", $el);
+							if ($others) {
+								$others .= ', ' . $el;
 							} else {
-								$el = $element;
+								$others = $el;
 							}
 						}
 					}
-					if ($el) {
-						$data['memo_admin'] .= ', ' . $el;
+					if ($others) {
+						$data['others'] .= ', ' . $others;
 					}
 					break;
 
-				case 'FotogalerieDetail bytu': //Фото
-					// echo "$i = $head[$i]";
-					$el = '';
+				case 'ФотогалереяИнформацияо квартире': //Foto
+					$el = $element->text();
+					$data['foto'] = $el;
 					break;
 			}
-
-			// Тут всегда активные объявления
-			// $data['active_parser'] = 1;
-
-			// Тип недвижимости (2 - квартира, 3 - дом, 4 - коммерческая недвижимость)
-			$data['type_id'] = 2;
-
-			// Номер парсера
-			$data['parser_id'] = $parser_id;
-
-			// Номер проекта
-			$data['project_id'] = 41;
-
-			// Формируем уникальное поле для записи
-			$data['unique_id'] = $data['parser_id']
-				. '-' . $data['property_number'];
 
 			++$i;
 
 			if ($i == ($num_last_td + 1)) {
 				$i = 1;
-				$this->list[] = $data;
-				$data = $this->initData();
+				$list[] = $data;
+				$data = array();
 			}
 		}
 
-		return $this->list;
+		return $list;
 	}
